cee6c546ebace8ebc596908fbf13927e06600b3e
Roman.Muzafarov@urfu.me <Roman.Muzafarov@urfu.me>
2021-03-21 12:00:31 +0500

    First commit

---
+++ TopCompanyPayload/Employees.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopCompanyPayload
+{
+    public class Employees
+    { 
+        string name; 
+        string position;
+        int hours;
+        decimal moneyOnHour; 
+
+        public Employees (string position, string name, int hours, decimal moneyOnHour)
+        {
+            this.position = position;
+            this.hours = hours;
+            this.moneyOnHour = moneyOnHour;
+            this.name = name; 
+        }
+
+        public void GetListEmp ()
+        {
+            string path = @"C:\Users\ADMIN\Desktop\Зарплаты\Список_Сотрудников.txt";
+            using (StreamReader str = new StreamReader(path, true))
+            {
+                string line;
+                while ((line = str.ReadLine()) != null)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+        }
+
+        public void AddHour (Employees em)
+        {
+            em.GetListEmp(); 
+
+        }
+
+        public void AddInDataBase (Employees a)
+        {
+            string path = @"C:\Users\ADMIN\Desktop\Зарплаты\Список_Сотрудников.txt";
+            using (StreamWriter stream = new StreamWriter(path, true))
+                stream.WriteLine($"{a.name} --- {a.position}"); 
+        }
+    }
+}
---
+++ TopCompanyPayload/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace TopCompanyPayload
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            string position = Console.ReadLine();
+            int hours = 0; 
+            decimal moneyOnHour = 0;
+            Employees st1 = new Employees(name, position, hours, moneyOnHour);
+            st1.AddInDataBase(st1);
+            Console.ReadLine();
+            st1.GetListEmp(); 
+        }
+    }
+}
